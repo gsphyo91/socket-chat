c72cff2bec306cdfefa1e4a34730486236954d87
Gyungsoo Phyo <gsphyo91@gmail.com>
2020-06-10 16:02:28 +0900

    added socket chat server

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const PORT = 3002;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to socket app ! - project starfruit");
+});
+
+io.on("connection", (socket) => {
+  var roomId = '';
+  var userName = '';
+  console.log("New Client Connected");
+  socket.on("init", (data) => {
+    console.log(data);
+    socket.join(data.seq);
+    roomId = data.seq;
+    userName = data.userName;
+    socket.broadcast.to(roomId).emit("welcome", data);
+  });
+
+  socket.on("message", (data) => {
+    console.log("message : " + data);
+    socket.emit("response", data);
+    socket.broadcast.to(roomId).emit("broadcast", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    socket.broadcast.to(roomId).emit("announce_dis", userName);
+    socket.leave(roomId);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("server is running");
+});
