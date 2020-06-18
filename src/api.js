93fe25b19423aaaf591599e64cb423ee9ed6a832
Gyungsoo Phyo <gsphyo91@gmail.com>
2020-06-18 17:19:17 +0900

    modified participant list

--- src/api.js
+++ src/api.js
@@ -1,3 +1,35 @@
-const axios = require('axios');
+const axios = require("axios");
+const config = require("../config/config.json");
 
-// 참여
+const SERVER_URL = config.SERVER_URL;
+
+// 채팅방 참여
+exports.joinToRoom = (liveSeq, userName) => {
+  return axios.post(`${SERVER_URL}/api/live/join`, {
+    liveSeq,
+    userName,
+  });
+};
+
+exports.leaveToRoom = (liveSeq, userName) => {
+  return axios.post(`${SERVER_URL}/api/live/withdraw`, {
+    liveSeq,
+    userName
+  })
+}
+
+exports.getParticipantList = (liveSeq) => {
+  return axios.get(`${SERVER_URL}/api/live/users`, {
+    params: {
+      liveSeq,
+    },
+  }).then(resp => {
+    return {
+      participantList : resp.data.body
+    }
+  }).catch(err => {
+    return {
+      err
+    }
+  });
+};
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const app = require("express")();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const api = require("./api");
+const { join } = require("path");
 
 const PORT = 3002;
 
@@ -8,17 +10,42 @@ app.get("/", (req, res) => {
   res.send("Welcome to socket app ! - project starfruit");
 });
 
+const joinToRoom = async (liveSeq, userName) => {
+  try {
+    const {data} = await api.joinToRoom(liveSeq, userName);
+    console.log(data);
+    const result = await api.getParticipantList(liveSeq);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const leaveToRoom = async (liveSeq, userName) => {
+  try{
+    const {data} = await api.leaveToRoom(liveSeq, userName);
+    console.log(data);
+    const result = await api.getParticipantList(liveSeq);
+    return result
+  }catch(err){
+    console.log(err);
+  }
+}
+
 io.on("connection", (socket) => {
   var roomId = "";
   var userName = "";
   console.log("New Client Connected");
-  socket.on("init", (data) => {
+  socket.on("init", async (data) => {
     try {
       console.log(data);
       socket.join(data.liveSeq);
       roomId = data.liveSeq;
       userName = data.userName;
+      const participantList = await joinToRoom(roomId, userName);
+      console.log(participantList);
       socket.broadcast.to(roomId).emit("welcome", data);
+      io.to(roomId).emit("participant", participantList);
       console.log(io.nsps["/"].adapter.rooms[roomId]);
     } catch (err) {}
   });
@@ -30,9 +57,13 @@ io.on("connection", (socket) => {
     socket.broadcast.to(roomId).emit("broadcast", data);
   });
 
-  socket.on("disconnect", () => {
+  socket.on("disconnect", async () => {
     console.log("Client disconnected");
+    console.log(roomId, userName);
+    const participantList = await leaveToRoom(roomId, userName);
+    console.log(participantList);
     socket.broadcast.to(roomId).emit("announce_dis", userName);
+    io.to(roomId).emit("participant", participantList);
     socket.leave(roomId);
   });
 });
