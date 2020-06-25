f4f22e7d057acb24ea1ad08d69cc8add0d14fbf0
Gyungsoo Phyo <gsphyo91@gmail.com>
2020-06-25 18:27:31 +0900

    modified get participant list

--- src/server.js
+++ src/server.js
@@ -10,27 +10,46 @@ app.get("/", (req, res) => {
   res.send("Welcome to socket app ! - project starfruit");
 });
 
+const getParticipantList = async (liveSeq) => {
+  try {
+    const participantList = await api.getParticipantList(liveSeq);
+    return participantList;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const joinToRoom = async (liveSeq, userName) => {
   try {
-    const {data} = await api.joinToRoom(liveSeq, userName);
-    console.log(data);
-    const result = await api.getParticipantList(liveSeq);
-    return result;
+    const participant = await api
+      .joinToRoom(liveSeq, userName)
+      .then(async (resp) => {
+        console.log("join");
+        console.log(resp.data);
+        const result = await getParticipantList(liveSeq);
+        return result;
+      });
+    return participant;
   } catch (err) {
     console.log(err);
   }
 };
 
 const leaveToRoom = async (liveSeq, userName) => {
-  try{
-    const {data} = await api.leaveToRoom(liveSeq, userName);
-    console.log(data);
-    const result = await api.getParticipantList(liveSeq);
-    return result
-  }catch(err){
+  try {
+    const participant = await api
+      .leaveToRoom(liveSeq, userName)
+      .then(async (resp) => {
+        console.log("[leave]");
+        console.log(resp.data);
+        const result = await getParticipantList(liveSeq);
+        return result;
+      });
+    return participant;
+  } catch (err) {
     console.log(err);
   }
-}
+};
 
 io.on("connection", (socket) => {
   var roomId = "";
@@ -42,10 +61,18 @@ io.on("connection", (socket) => {
       socket.join(data.liveSeq);
       roomId = data.liveSeq;
       userName = data.userName;
+      // await joinToRoom(roomId, userName).then((resp) => {
+      //   console.log("[participantList]");
+      //   console.log(resp);
+      //   io.to(roomId).emit("participant", resp);
+      // });
       const participantList = await joinToRoom(roomId, userName);
       console.log(participantList);
-      socket.broadcast.to(roomId).emit("welcome", data);
+      // const participantList = await joinToRoom(roomId, userName);
+      // console.log('[participantList]');
+      // console.log(participantList);
       io.to(roomId).emit("participant", participantList);
+      socket.broadcast.to(roomId).emit("welcome", data);
       console.log(io.nsps["/"].adapter.rooms[roomId]);
     } catch (err) {}
   });
